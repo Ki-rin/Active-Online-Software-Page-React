3aae0b8bf7c74f89c22f204ebd636736d067829c
Ki Rin <miriam.a.silver@gmail.com>
2023-02-20 10:00:17 +0200

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './style.css'
-import Home from './views/home'
+import Dgen from './views/dgen'
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Route component={Home} exact path="/" />
+        <Route component={Dgen} exact path="/" />
       </div>
     </Router>
   )
